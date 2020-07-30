4b6776281ad42dc0d1755d93b2f89bf08490c238
dr-matt-smith <matt.smith@itb.ie>
2020-07-31 00:50:39 +0100

    added files to project

---
+++ Assets/_Scripts/MoveBird.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoveBird : MonoBehaviour
+{
+    public float speed = 1f;
+
+    void Update()
+    {
+        transform.Translate(speed * Vector3.right * Time.deltaTime);
+    }
+}
